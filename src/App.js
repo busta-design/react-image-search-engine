07e56d3c09af2bdbe3cff4c628f361f86501c6b0
elpep <andresbusta4444@gmail.com>
2021-02-09 20:47:06 -0400

    Search component mounted

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+//https://image-search-3d130.web.app
+import React, { Component } from 'react';
+import Searcher from './components/Searcher'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  state = {
+    termino: ""
+  }
+
+  datosBusqueda = termino => {
+    this.setState({
+      termino
+    })
+  }
+
+  render() {
+    return (
+      <div className="app container">
+        <div className="jumbotron">
+          <p className="lead text-center">Buscador de imagenes</p>
+
+          <Searcher 
+            datosBusqueda={this.datosBusqueda}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
